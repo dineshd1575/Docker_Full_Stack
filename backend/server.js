7a18009ca8339eb0fa2673b0bd11155a3c152270
dineshd1575 <dineshdhanapal04@gmail.com>
2026-07-08 12:24:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,5 +39,5 @@ app.delete("/tasks/:id", async (req, res) => {
   res.json({ message: "Task deleted" });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server running on " + port));
